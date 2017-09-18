3917014c49a96484db92d3c253a1bce533af8ab4
sebastiaangroeninkDeltion <sebastiaangroenink@gmail.com>
2017-09-18 22:08:10 +0200

    fixing shit

---
+++ Unity file/Neon Tower Defense Game Neon Edition 2k17/Assets/CreatePath.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePath : MonoBehaviour {
+
+    public void OnCollisionStay(Collision col)
+    {
+        if(col.gameObject.tag == "Tile")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        print("1");
+        if (col.gameObject.tag == "Tile")
+        {
+            print("2");
+            Destroy(col.gameObject);
+        }
+    }
+}
